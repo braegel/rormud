d7dad858a94f10da8c05d4f7462fc82d1b304e08
Bernd Braegelmann <bernd@braegelmann.de>
2011-10-08 20:44:31 +0200

    initial commit after rails new rormud

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rormud::Application.config.secret_token = 'b9786a75b9c9d443c9bd8809e4260ca22a3e5b88f887eb15a9ce65d056af69da56cf5eec4d9d0aefb4b114b8740944ee02726858bb54b01f9ac9b080701893db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rormud::Application.config.session_store :cookie_store, key: '_rormud_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rormud::Application.config.session_store :active_record_store
