64d6d3cdb1b4caeddf49f182ef979812c1706652
Bernd Braegelmann <bernd@braegelmann.de>
2011-10-09 00:15:35 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Room.delete_all
+
+Room.create(
+  :name => "Willkommen",
+  :description => "Es ist total dunkel"
+)
